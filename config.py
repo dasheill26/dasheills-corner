8ff189667a2b12780ce2b1029626967c1741bfa2
dasheill26 <dasheillknicxvas@gmail.com>
2026-02-02 21:29:12 +0000

    Security: harden session cookie settings

--- config.py
+++ config.py
@@ -16,3 +16,6 @@ class Config:
     BABEL_DEFAULT_LOCALE = "en"
     BABEL_SUPPORTED_LOCALES = ["en", "es", "fr"]
     BABEL_TRANSLATION_DIRECTORIES = "translations"
+
+    SESSION_COOKIE_HTTPONLY = True
+    SESSION_COOKIE_SAMESITE = "Lax"
